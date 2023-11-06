fecc65bd0bc31413fb186a505785edea8fb0edde
NathanJLee <128037452+NathanJackyLee@users.noreply.github.com>
2023-11-06 20:30:50 +0700

    Difficulty 1,2,3

---
+++ HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index(){
+        // wipe session to default state
+        session(['game_running'=>false]);
+        session(['round'=>0]);
+        session(['score'=>0]);
+        session(['lives'=>0]);
+        session(['difficulty'=>0]);
+        session(['question'=>'']);
+        session(['answer'=>'']);
+
+        return view('home');
+    }
+}
